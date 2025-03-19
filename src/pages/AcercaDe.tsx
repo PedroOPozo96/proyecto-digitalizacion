c410813aa47d4f3fc6b11515356a1a31213f2e59
PedroOPozo96 <168337714+PedroOPozo96@users.noreply.github.com>
2025-03-19 10:35:46 +0100

    Update AcercaDe.tsx

--- src/pages/AcercaDe.tsx
+++ src/pages/AcercaDe.tsx
@@ -96,6 +96,7 @@ const AcercaDe = () => {
             <div className="max-w-6xl mx-auto">
               <div className="text-center mb-12">
                 <h2 className="text-3xl font-bold text-gray-900 mb-4"> <a href="https://www.youtube.com/watch?v=3wRNM0z5nxk" target="_blank" rel="noopener noreferrer" Nuestro Video Corporativo</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/3wRNM0z5nxk?si=DqZL579t76D_Aclt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 <div className="w-20 h-1 bg-blue-600 mx-auto"></div>
               </div>
               <div className="bg-white p-8 rounded-lg shadow-lg">
